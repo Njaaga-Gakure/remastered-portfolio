786ed3bbea8c7391c9a5ddbd6c218ec729876c45
Njaaga-Gakure <50949648+Njaaga-Gakure@users.noreply.github.com>
2023-07-18 09:22:03 +0300

    Use the nanoid library to get unique ids

--- src/utils/data.jsx
+++ src/utils/data.jsx
@@ -7,39 +7,50 @@ import {
   BiLogoMongodb,
 } from "react-icons/bi";
 import { SiExpress } from "react-icons/si";
+import { nanoid } from "nanoid";
 
 const skillsUsingData = [
-  { id: 1, name: "HTML5", icon: <AiFillHtml5 />, iconColor: "#e34c26" },
-  { id: 2, name: "CSS3", icon: <FaCss3Alt />, iconColor: "#264de4" },
+  { id: nanoid(), name: "HTML5", icon: <AiFillHtml5 />, iconColor: "#e34c26" },
+  { id: nanoid(), name: "CSS3", icon: <FaCss3Alt />, iconColor: "#264de4" },
   {
-    id: 3,
+    id: nanoid(),
     name: "JavaScript",
     icon: <BiLogoJavascript />,
     iconColor: "#F0DB4F",
   },
-  { id: 4, name: "React js", icon: <FaReact />, iconColor: "#61dbfb" },
+  { id: nanoid(), name: "React js", icon: <FaReact />, iconColor: "#61dbfb" },
 ];
 const skillsLearningData = [
   {
-    id: 1,
+    id: nanoid(),
     name: "TypeScript",
     icon: <BiLogoTypescript />,
     iconColor: "#007acc",
   },
   {
-    id: 2,
+    id: nanoid(),
     name: "TailWind",
     icon: <BiLogoTailwindCss />,
     iconColor: "#4dc0b5",
   },
   {
-    id: 3,
+    id: nanoid(),
     name: "nodejs",
     icon: <FaNodeJs />,
     iconColor: "#3C873A",
   },
-  { id: 4, name: "expressjs", icon: <SiExpress />, iconColor: "#303030" },
-  { id: 5, name: "mongodb", icon: <BiLogoMongodb />, iconColor: "#4DB33D" },
+  {
+    id: nanoid(),
+    name: "expressjs",
+    icon: <SiExpress />,
+    iconColor: "#303030",
+  },
+  {
+    id: nanoid(),
+    name: "mongodb",
+    icon: <BiLogoMongodb />,
+    iconColor: "#4DB33D",
+  },
 ];
 
 export { skillsUsingData, skillsLearningData };
